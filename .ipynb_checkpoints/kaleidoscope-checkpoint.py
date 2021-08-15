ee37d820bd9acc37d9c82fa1722f40b559328c08
Alina Krause <alikrause@uni-osnabrueck.de>
2021-08-15 11:31:05 +0200

    adjustments

--- .ipynb_checkpoints/kaleidoscope-checkpoint.py
+++ .ipynb_checkpoints/kaleidoscope-checkpoint.py
@@ -4,78 +4,86 @@ import matplotlib.animation as animation
 import snowflake as sn
 from IPython.display import HTML
 import copy
-import math
-from PIL import Image
 import random
 
+"""
+Koch Snowflake Kaleidoscope
 
-#A list for the coordinates of each snowflake
+Final Project for Scientific Programming in Python in SS 2021
+
+by Pia Schröter, Alina Krause and Paula Heigl
+"""
+
+
+# global variables
+# list for the coordinates of each snowflake
 snow_list = []
-#A list for all the plotted coordinates
+# list for all the plotted coordinates
 lines = []
-rotate = False
 
-colour = 1
-
-    
+   
 def main():
-    global colour
     # user input
+    # set complexity of kaleidoscope / how many snowflakes
     global amount
-    name = input("What's your name?")
-    amount = int(input(name + ", how big do you want your kaleidosocope to be? You can choose from a number from 1 to 5\n"))
+    name = input("What's your name? \n")
+    amount = int(input(name + ", how complex do you want the kaleidoscope to be? You can choose from a number from 1 to 5\n"))
     while amount < 1 or amount > 5:
-        amount = int(input("Please indicate the size of the kaleidoscope with a number from 1 to 5!"))
-    colour = int(input("What colour do you want for your kaleidoscope?\n1 red,\n2 yellow,\n3 green,\n4 grey,\n5 blue or\n6 purple?\n"))
+        amount = int(input("Please indicate the complexity of your kaleidoscope with a number from 1 to 5!"))
+    
+    # set colour theme
+    global colour
+    colour = int(input("Okay, now what colour do you want for your kaleidoscope?\n1 red,\n2 yellow,\n3 green,\n4 grey,\n5 blue or\n6 purple?\n"))
     while colour < 1 or colour > 6:
         colour = int(input("Please indicate which colour you would like with a number from 1 to 6! "))
+    
+    # set rotation
+    global rotate
     turn = input("Do you want the snowflake to rotate? Yes or No?\n")
-    if  turn == "yes" or turn == "Yes":
-        global rotate
+    if turn == "yes" or turn == "Yes":
         rotate = True
-    speed = int(input("How fast from 1 to 10 do you want your kaleidoscope to change? "))
+    else:
+        rotate = False
+        
+    # set speed
+    speed = int(input("How fast from 1 to 10 do you want your kaleidoscope to change, " + name + "? \n"))
     while speed < 1 or speed > 10:
         speed = int(input("Please indicate your preferred speed with a number from 1 to 10! "))
+        
+    print("I will generate your kaleidoscope now...")
    
     
-    #Settings for the plot
+    # settings for the plot
     plt.style.use("dark_background")
     fig, ax = plt.subplots()
     plt.axis('off')
+    line, = ax.plot([],[]) # the plot is empty for the animation
 
-    #fills the snow-list with snowflakes
+    # fill snow_list with the coordinates relevant for the koch snowflakes
     snow_iterator(amount)
-  
+      
     
-    #the plot is empty for the animation
-    line, = ax.plot([],[])
-    
-    
-    #the list will be filled with same amount of empty plots as needed in the end
-    #c is used to change the values of the colours
+       
+    # fill lines list with same amount of empty plots as triangles in snow_list
+    # the colour gets lighter with each line 
     c = 0
-    #A List of plots to have multiple lines
-    #The colour gets lighter with each line 
     for flakes in snow_list:
         c += 1
         l = ax.plot([],[], 1)[0]
         lines.append(l)
     
-    
-
+    # generate the animation and safe as gif
     anim = animation.FuncAnimation(fig, animate, init_func=init, frames=10, interval=1100-(speed*100), blit= True)
     anim.save("kaleidoscope.gif", name)
-    # for nomal python (doesn't work with jupyter lab)
-    #plt.show()
+    #plt.show() # doesn't work in jupyter lab 
     
     print("You can find your personalized kaleidoscope in this folder! Have fun :)")
     
     
-# Function to create the coordinates for each snowflake
-#Das kann vielleicht für die Rotation in die animate-function
+# function to create the coordinates for each snowflake
 def snow_iterator(x):
     for i in range(x):
-
+        
         if(i>=3):
             snow_list.append([[[0.36- (2**(i*((2.1/(i+2))*i)))*0.35,0.15-(2**(i*((2.1/(i+2))*i)))*0.21],[0.54,0.45+(2**(i*((2.1/(i+2))*i)))*0.42],[0.72+(2**(i*((2.1/(i+2))*i)))*0.36,0.15-(2**(i*((2.1/(i+2))*i)))*0.21]],1])
             snow_list.append([[[0.42-(2**(i*((2.1/(i+2))*i)))*0.23,0.25],[0.6+(2**(i*((2.1/(i+2))*i)))*0.12,0.35+(2**(i*((2.1/(i+2))*i)))*0.21],[0.6+(2**(i*((2.1/(i+2))*i)))*0.12,0.15-(2**(i*((2.1/(i+2))*i)))*0.21]],1])
@@ -85,63 +93,86 @@ def snow_iterator(x):
             snow_list.append([[[0.42-(i**(2))*0.23,0.25],[0.6+(i**(2))*0.12,0.35+(i**(2))*0.21],[0.6+(i**(2))*0.12,0.15-(i**(2))*0.21]],1])
 
         
-#Default for every line
+# default for every line
 def init():     
     for line in lines:
         line.set_data([],[])
     return lines
 
 
+# change the coordinates of all points in snow_list by 12° around the center
+def rotation():
+    cx = 0.54 # coordinates of the center of the snowfalkes
+    cy = 0.25
+    radian = np.radians(12) # angle
 
-#Method to draw and animate each snowflake
+    global snow_list
+    new_snow_list = copy.deepcopy(snow_list)
+
+    # iterate trough snow_list and rotate each coordinates by 12°
+    for pair in range(len(new_snow_list)):
+        for point in range(len(new_snow_list[pair][0])):
+            new_snow_list[pair][0][point][0] = ((snow_list[pair][0][point][0]-cx) * np.cos(radian)) + ((snow_list[pair][0][point][1]-cy) * np.sin(radian)) + cx
+            new_snow_list[pair][0][point][1] = -((snow_list[pair][0][point][0]-cx) * np.sin(radian)) + ((snow_list[pair][0][point][1]-cy) * np.cos(radian)) + cy
+
+    snow_list = copy.deepcopy(new_snow_list)
+
+
+
+# method to draw and animate each snowflake
 def animate(i):  
-    # change the points in snow_list to make the snowflake rotate
-    if rotate == True:
-        global snow_list
-        # coordinates of the center
-        x = 0.54
-        y = 0.25
-        new_snow_list = copy.deepcopy(snow_list)
-        for pair in range(len(new_snow_list)):
-            for point in range(len(new_snow_list[pair][0])):
-                new_snow_list[pair][0][point][0] = ((snow_list[pair][0][point][0]-x) * math.cos(0.20943951)) + ((snow_list[pair][0][point][1]-y) * math.sin(0.20943951)) + x
-                new_snow_list[pair][0][point][1] = -((snow_list[pair][0][point][0]-x) * math.sin(0.20943951)) + ((snow_list[pair][0][point][1]-y) * math.cos(0.20943951)) + y
-        snow_list = copy.deepcopy(new_snow_list)
-        
+    
+    # change the points in snow_list in each frame to make the snowflake rotate
+    if rotate == True:  
+        rotation()
+          
+    # lists for ?
     xList = []
     yList = []
+    
+    # iterate through each triangle coordinates
     for triangle, k in snow_list:
+        # initially snowflake gets more and
+        # after half of the frames it gets less branched
         if i < 7:
             k = i
-        if i > 6:
+        else:
             k = 10 - i
         
+        # calculate the koch snowflakes
         data = np.array(sn.snow(triangle, k))
         x, y = np.split(data, 2, axis=1)        
         xList.append(x)
         yList.append(y)
+        
+        # ?
         if(k == 0):
             x_sorted = np.sort(np.concatenate(xList).ravel())
             y_sorted = np.sort(np.concatenate(yList).ravel())
             plt.xlim(x_sorted[0]-((0.07*amount) * amount), x_sorted[-1]+ ((0.07*amount) * amount **2))
             plt.ylim(y_sorted[0]-(((2**-(6-amount)) * (4))*amount), y_sorted[-1] + 0.05)
     
+    # set the colour for all lines
     for lnum,line in enumerate(lines):
         line.set_data(xList[lnum], yList[lnum])
+        
         global colour
-        if colour == 5:
-            line.set_color(color = (0,random.uniform(0, 0.2),random.uniform(0.2, 1))) #blau
-        elif colour == 3:
-            line.set_color(color = (0,random.uniform(0.6,0.9),random.uniform(0.2,0.6))) #grün
-        elif colour == 6:
-            line.set_color(color =(0.6,random.uniform(0,0.4), random.uniform(0,0.9))) #lila
-        elif colour == 1:
+        
+        
+        if colour == 1:
             line.set_color(color = (1,random.uniform(0,0.3),random.uniform(0,0.7))) #rot
         elif colour == 2:
             line.set_color(color = (1,random.uniform(0.8,1),random.uniform(0.2,0.9))) #gelb
+        elif colour == 3:
+            line.set_color(color = (0,random.uniform(0.6,0.9),random.uniform(0.2,0.6))) #grün  
         elif colour == 4:
             val = random.uniform(0.1,0.9)
-            line.set_color(color = (val,val,val)) #grau
+            line.set_color(color = (val,val,val)) #grau    
+        elif colour == 5:
+            line.set_color(color = (0,random.uniform(0, 0.2),random.uniform(0.2, 1))) #blau   
+        elif colour == 6:
+            line.set_color(color =(0.6,random.uniform(0,0.4), random.uniform(0,0.9))) #lila
+        
     
     return lines
         
--- snowflake.py
+++ snowflake.py
@@ -1,35 +1,37 @@
 import numpy as np
 
+"""
+Reference: https://programmer.group/use-matplotlib-to-draw-snowflakes-and-snow-scenes.html
+"""
 
 
-def rotate(p, d):
-    """Return point p Rotate counterclockwise around the origin d Coordinates of degree"""
-    
-    a = np.radians(d)
-    m = np.array([[np.cos(a), np.sin(a)],[-np.sin(a), np.cos(a)]])
-    return np.dot(p, m)
-
+# generate the three additional points (u,v,w) for the koch curve of a line pq
 def koch_curve(p, q):
-    """Line segment pq Generate koch curve, return to uvw Three points"""
-    
     p, q = np.array(p), np.array(q)
-    u = p + (q-p)/3 # The coordinates of the third point u
-    v = q - (q-p)/3 # The coordinate of the third point V
-    w = rotate(v-u, 60) + u # The coordinate of point w can be obtained by turning the line uv around the u point anticlockwise by 60 °
     
+    u = p + (q-p)/3 
+    v = q - (q-p)/3 
+    
+    # w = turning the line uv around the u point anticlockwise by 60°
+    angle = np.radians(60)
+    w = np.dot(v-u, np.array([[np.cos(angle), np.sin(angle)],[-np.sin(angle), np.cos(angle)]])) + u
+   
     return u.tolist(), v.tolist(), w.tolist()
     
-def snow(triangle, k):
-    """Given a triangle, a closed Koch snowflake is generated"""
     
-    for i in range(k):
+# generate koch snowflake for a triangle with certain depth
+def snow(triangle, depth):
+    
+    for i in range(depth):
         result = list()
         t_len = len(triangle)
+        
         for j in range(t_len):
             p = triangle[j]
             q = triangle[(j+1)%t_len]
             u, v, w = koch_curve(p, q)
             result.extend([p, u, w, v])
+            
         triangle = result.copy()
     
     triangle.append(triangle[0])
